a040280cde1bead05da9d16f6e77c09d59fef53e
Maxim <112863178+grilld9@users.noreply.github.com>
2024-03-05 10:31:48 +0700

    Сделал валидацию для имен классов и параметров конструктора

---
+++ di_framework/src/main/java/ru/nsu/fit/context/JsonContextCreator.java
@@ -0,0 +1,54 @@
+package ru.nsu.fit.context;
+
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import ru.nsu.fit.model.BeanDefinition;
+import ru.nsu.fit.parsing.ConfigurationParser;
+
+@Slf4j
+public class JsonContextCreator implements ContextCreator {
+
+    @Override
+    public List<BeanDefinition> createContext() {
+        List<BeanDefinition> beans = ConfigurationParser.parse(Paths.get("src/main/resources/application.json"));
+        for (BeanDefinition bean : beans) {
+            Class<?> aClass = validateByClassName(bean.getClassName());
+            validateConstructorParams(bean.getConstructorParams(), aClass);
+        }
+        return beans;
+    }
+
+    private void validateConstructorParams(List<String> constructorParams, Class<?> aClass) {
+        Set<Class<?>> paramsTypes = constructorParams
+            .stream()
+            .map(this::validateByClassName)
+            .collect(Collectors.toSet());
+        if (Arrays.stream(aClass.getConstructors())
+            .noneMatch(constructor ->
+                Arrays.stream(constructor.getParameterTypes())
+                .collect(Collectors.toSet())
+                .equals(paramsTypes))) throw new RuntimeException("Конструктор "
+            + "с параметрами " + constructorParams
+            + " не найден в классе " + aClass.getName() );
+    }
+
+    private Class<?> validateByClassName(String className) {
+        return new Reflections("ru.nsu.fit", new SubTypesScanner(false))
+            .getSubTypesOf(Object.class)
+            .stream()
+            .filter(aClass -> aClass.getName().equals(className))
+            .findAny().orElseThrow(() -> new RuntimeException("Класса " + className
+                + " не существует"));
+    }
+
+    @Override
+    public List<BeanDefinition> createContext(String packageName) {
+        return null;
+    }
+}
--- di_framework/src/main/java/ru/nsu/fit/parsing/ConfigurationParser.java
+++ di_framework/src/main/java/ru/nsu/fit/parsing/ConfigurationParser.java
@@ -1,7 +1,10 @@
 package ru.nsu.fit.parsing;
 
+import static org.reflections.Reflections.log;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import java.io.UncheckedIOException;
 import ru.nsu.fit.model.BeanDefinition;
 
 import java.io.BufferedReader;
@@ -13,17 +16,15 @@ import java.util.List;
 
 public class ConfigurationParser {
 
-    private final Gson gson;
-
-    public ConfigurationParser() {
-        gson = new Gson();
-    }
-
-    public List<BeanDefinition> parse(Path path) throws IOException {
+    public static List<BeanDefinition> parse(Path path) {
+        Gson gson = new Gson();
         List<BeanDefinition> beans;
         try (BufferedReader reader = new BufferedReader(new FileReader(path.toString()))) {
             TypeToken<List<BeanDefinition>> beanListType = new TypeToken<>(){};
             beans = gson.fromJson(reader, beanListType);
+        } catch (IOException e) {
+            log.error("Ошибка чтения файла конфигурации: {}", e.getMessage());
+            throw new UncheckedIOException(e);
         }
         return beans;
     }
